108eb45623630e4215b8624faeb4261aff2b2560
swagfinger <swagfinger@gmail.com>
2023-04-03 11:30:30 +0800

    root renders App

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+https://github.com/swagfinger/webpack-template
 https://github.com/swagfinger/swagfinger-testing-react-router-6.git
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import App from './App';
 
 import { createRoot } from 'react-dom/client';
 // Clear the existing HTML content
@@ -6,4 +7,4 @@ document.body.innerHTML = '<div id="app"></div>';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+root.render(<App />);
